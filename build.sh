3ae24b655cf1e0fa07155f35335171875e1fc9bc
Matthew Sheppard <matthew.sheppard@instaclustr.com>
2022-02-10 12:41:05 +1100

    Add newline to end of build.sh to address warning

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG marpteam/marp-cli --html d
 docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG marpteam/marp-cli --html docs/slides.md --pptx -o target/slides.pptx
 docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG marpteam/marp-cli --html docs/slides.md --html -o target/slides.html
 popd
+
